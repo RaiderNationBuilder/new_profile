3336dd32c176eba8cdbf71877b8ea64bbb4d06eb
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2022-08-20 09:34:04 -0500

    built sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,59 @@
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faDna, faAt, faFile } from '@fortawesome/free-solid-svg-icons'
+import { NavLink, Link } from 'react-router-dom'
+
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      {/* setting up side bar links with fontawesome icons*/}
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faDna} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faAt} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://docs.google.com/document/d/1JpgyNycM65gmBZpKojnhcKjlUTcbBgB7D1UJ9mwSPBs/edit?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faFile} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/patrick-sebstead-6980277a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/RaiderNationBuilder"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
